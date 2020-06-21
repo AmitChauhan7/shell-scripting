d91983becc5331fdeb0bb91e64f3f441d236d0d3
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-06-21 17:11:07 -0400

    Adding sample.txt- Adding to clone

---
+++ 01-she-bang.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# SH - Bourne Shell -> HPUX/ AT&T  -> Simple Syntax
+# KSH - Korn Shell -> IBM          -> Login Scripts
+# CSH - C Shell                    -> Index Variables
+
+# BASH -> Again Shell / Bourne Again Shell
+# BASH = SH + KSH + CSH, Default in Linux
+
+# ZSH -> Desktop Shell
+
+# She-Bang -> #!
+#   - Denotes which script it is.
+#   - Ensures the remaining code executing using that interpreter
