fa61efc7b1831a67f51467546d323d225de19de5
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-06-21 18:30:32 -0400

    Practicing

--- 02-print.sh
+++ 02-print.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-# Printing can be done i two commands
-# 1. echo
-# 2. Printf
+## Printing can be done i two commands
+## 1. echo
+## 2. Printf
+#
+## We choose the echo command: user friendly & less syntax to do teh job
+## syntax: echo INPUT-MESSAGE
+#
+#echo Hello World
+#
+## Print Multi Lined Output
+#
+## syntax: echo -e "Line1\nLine"
+## Observation: -e option is mandatory, \n prints new line, Double quotes are mandatory
+#
+#echo -e "Hello, \nGood Morning, \nWelcome to DevOps"
 
-# We choose the echo command: user friendly & less syntax to do teh job
-# syntax: echo INPUT-MESSAGE
+echo -e "\e[31mHello in RED Color\e[0m"
+echo -e "\e33mHello in Yellow Color\e[0m"
+echo Hello with NO color
 
-echo Hello World
-
-# Print Multi Lined Output
-
-# syntax: echo -e "Line1\nLine"
-# Observation: -e option is mandatory, \n prints new line, Double quotes are mandatory
-
-echo -e "Hello, \nGood Morning, \nWelcome to DevOps"
+echo -e "\e[42;36mHello\e[0m"
