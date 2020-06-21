31b6055efa188dffde904989c7d29daf54d79433
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-06-21 17:57:56 -0400

    Practicing

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Printing can be done i two commands
+# 1. echo
+# 2. Printf
+
+# We choose the echo command: user friendly & less syntax to do teh job
+# syntax: echo INPUT-MESSAGE
+
+echo Hello World
