31d1f9fd4329d4d30280022b9129b8d8049263c6
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-06-21 18:57:55 -0400

    Practicing

--- 03-vars.sh
+++ 03-vars.sh
@@ -7,3 +7,10 @@ d=true
 e=1.233
 
 echo valus of a = ${a}
+
+# Static Date
+DATE=2020-06-21
+echo "Good Morning, Today's date is ${DATE}"
+
+DATE1=$(date)
+echo "Good Morning, Todays date is ${DATE1}"
