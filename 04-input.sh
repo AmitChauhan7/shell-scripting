cb81041311e40c44ce32eebe4a61aaac1925f116
Amit Chauhan <er.amitkchauhan@gmail.com>
2020-06-21 19:10:58 -0400

    Practicing

--- 04-input.sh
+++ 04-input.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
 # During execution
+<<COMMENTED
 read -p "ENter your name: " name
 echo "Name = $name"
+COMMENTED
+
+# Before Execution
+# With special variables we can take the input
+
+echo "Script Name = $0"
+echo "First argument = $1"
+echo "Second Args = $2"
+
+echo "All arguments = $*"
+echo "Number of arguments $#"
